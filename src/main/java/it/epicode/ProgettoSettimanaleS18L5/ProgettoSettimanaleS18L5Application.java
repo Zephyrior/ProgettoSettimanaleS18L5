235546b2b339d7fc9ffe669bff9c90b0547d17d9
Zephyrior <mark.temblique@gmail.com>
2025-04-18 10:55:50 +0200

    Creazione progetto

---
+++ src/main/java/it/epicode/ProgettoSettimanaleS18L5/ProgettoSettimanaleS18L5Application.java
@@ -0,0 +1,13 @@
+package it.epicode.ProgettoSettimanaleS18L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanaleS18L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanaleS18L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/ProgettoSettimanaleS18L5/ProgettoSettimanaleS18L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.ProgettoSettimanaleS18L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleS18L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
